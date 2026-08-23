bbc6bbbab08b1f802c17479335a19b8f90ebedb0
supastarrr777 <w6m8nkk7c9@privaterelay.appleid.com>
2026-08-23 21:09:03 +0300

    Update ContentView.swift

--- Source/ContentView.swift
+++ Source/ContentView.swift
@@ -69,7 +69,7 @@ struct ContentView: View {
             if phase == .active {
 
                 if let url = URL(
-                    string: "https://apple.com"
+                    string: "prefs:root=General&path=VPN"
                 ) {
 
                     openURL(url)
