c1813851b16dc18a2500b4e79fdc81c68b939e34
supastarrr777 <w6m8nkk7c9@privaterelay.appleid.com>
2026-08-23 17:13:49 +0300

    Create ToggleVPNIndicatorIntent.swift

---
+++ Source/AppIntents/ToggleVPNIndicatorIntent.swift
@@ -0,0 +1,15 @@
+import AppIntents
+
+struct ToggleVPNIndicatorIntent: AppIntent {
+
+    static let title: LocalizedStringResource = "Toggle VPN Indicator"
+
+    static let description = IntentDescription(
+        "Turns VPN Live Activity on or off."
+    )
+
+    func perform() async throws -> some IntentResult {
+        return .result()
+    }
+}
+`
